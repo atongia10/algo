b211f9f24839ec191272a073d77f6250a1d7e12a
atongia10 <atongia10@gmail.com>
2019-04-11 22:15:47 -0400

    Initial Commit

---
+++ src/main/java/com/algo/greedy/KnapsackFraction.java
@@ -0,0 +1,23 @@
+package com.algo.greedy;
+
+public class KnapsackFraction {
+
+    public static void main(String[] args) {
+
+        int [] value = {10, 40, 20, 30};
+        int [] weight = {60, 40, 100, 120};
+        int capacity = 50;
+        System.out.println("Optimum Maximum Value "+getOptimumMaxValue(value,weight,capacity));
+
+    }
+
+    private static double getOptimumMaxValue(int[] value, int[] weight, int capacity) {
+        double totalOptimum = 0.0;
+
+
+
+
+        return totalOptimum;
+    }
+
+}
